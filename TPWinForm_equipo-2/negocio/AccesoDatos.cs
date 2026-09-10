211e50d260636a62ab8e2b8bebdbd1ee74075c7d
Max <maxibian16@gmail.com>
2026-09-10 16:55:37 -0300

    Update AccesoDatos
    Se agrega el método cerrarConexion y se corrige el método ejecutarLectura

--- TPWinForm_equipo-2/negocio/AccesoDatos.cs
+++ TPWinForm_equipo-2/negocio/AccesoDatos.cs
@@ -13,12 +13,15 @@ namespace negocio
         private SqlConnection conexion;
         private SqlCommand comando;
         private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
 
         public AccesoDatos()
         {
             conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
-            comando = new SqlCommand();
-             
+            comando = new SqlCommand();    
         }
         public void setearConsulta(string consulta)
         {
@@ -28,8 +31,21 @@ namespace negocio
         public void ejecutarLectura()
         {
             comando.Connection = conexion;
-            conexion.Open();
-            lector = comando.ExecuteReader();
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            } 
+        }
+        public void cerrarConexion()
+        {
+            if(lector != null)
+                lector.Close();
+            conexion.Close();
         }
     }
 }
