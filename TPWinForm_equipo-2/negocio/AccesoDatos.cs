a3156e464928b923fb7afe64bd0d78f218185598
Max <maxibian16@gmail.com>
2026-09-10 16:49:27 -0300

    Creación clase AccesoDatos

---
+++ TPWinForm_equipo-2/negocio/AccesoDatos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+             
+        }
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            lector = comando.ExecuteReader();
+        }
+    }
+}
--- TPWinForm_equipo-2/negocio/ArticuloNegocio.cs
+++ TPWinForm_equipo-2/negocio/ArticuloNegocio.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using dominio;
 
 namespace negocio
 {
     public class ArticuloNegocio
     {
+        public List<Articulo> listarArticulos()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            try
+            {
+
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
     }
 }
