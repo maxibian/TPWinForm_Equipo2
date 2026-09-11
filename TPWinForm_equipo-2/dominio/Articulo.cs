2c143765cd75382548e3ccbaf578641adb127e82
Max <maxibian16@gmail.com>
2026-09-10 21:57:27 -0300

    Creación del método listarArticulos

--- TPWinForm_equipo-2/dominio/Articulo.cs
+++ TPWinForm_equipo-2/dominio/Articulo.cs
@@ -12,8 +12,9 @@ namespace dominio
         public string Codigo { get; set; }
         public string Nombre  { get; set; }
         public string Descripcion { get; set; }
-        public int IdMarca { get; set; }
-        public int IdCategoria { get; set; }
+        //public Marca Marca { get; set; }
+        //public Categoria Categoria { get; set; }
+        public string Imagen { get; set; }
         public decimal Precio { get; set; }
 
     }
--- TPWinForm_equipo-2/negocio/ArticuloNegocio.cs
+++ TPWinForm_equipo-2/negocio/ArticuloNegocio.cs
@@ -5,17 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using dominio;
+using System.Net;
 
 namespace negocio
 {
     public class ArticuloNegocio
     {
+        AccesoDatos datos = new AccesoDatos();
         public List<Articulo> listarArticulos()
         {
             List<Articulo> lista = new List<Articulo>();
             try
             {
+                datos.setearConsulta("Select.....");
+                datos.ejecutarLectura();
+                while(datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    //aux.IdMarca=(Marca)
+                    //aux.Categoria=(Categoria)
+                    aux.Imagen = (string)datos.Lector["Imagen"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
 
+                    lista.Add(aux);
+    }
 
                 return lista;
             }
